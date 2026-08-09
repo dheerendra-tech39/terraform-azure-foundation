fb965dec87759112953c0e72c496c9a548d52c50
unknown <www.dheerendraverma@gmail.com>
2026-08-10 00:44:34 +0530

    code added workflows

--- environment/dev/provider.tf
+++ environment/dev/provider.tf
@@ -8,6 +8,7 @@ terraform {
 
 
   backend "azurerm" {
+    subscription_id      = "9d42e269-3739-4811-a09c-a7fe1e4e9c7a"
     resource_group_name  = "RG-Dheeru39"
     storage_account_name = "stddheeru39"
     container_name       = "tfstate-container"
